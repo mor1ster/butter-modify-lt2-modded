8c938af3e6fd779b01e92b94881a7c16af2ebe5a
mor1ster <158586666+mor1ster@users.noreply.github.com>
2024-06-10 22:35:19 +0300

    Update lt2modded.lua

--- lt2modded.lua
+++ lt2modded.lua
@@ -20,9 +20,7 @@ pcall(function()
 end)
 ]]
 
-if game.PlaceId == 13822889 then -- 🌳 Lumber Tycoon 2
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Butterisgood/Butter/main/lt2'))("")
-elseif game.PlaceId == 15740639668 then -- (Winter!) The lumber magnate has been modified!
+if game.PlaceId == 15740639668 then -- (Winter!) The lumber magnate has been modified!
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Butterisgood/Butter/main/lt2'))("")
 elseif game.PlaceId == 6897226634 then -- Timber! 🌲
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Butterisgood/Butter/main/timber-obfuscated.lua'))("")
